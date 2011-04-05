adfc99aa1a1e624781aeeefdee9329e4a0934b23
hzc <farseer90718@gmail.com>
2011-04-05 16:32:14 +0800

    connector

---
+++ src/connector/org/clothocad/widget/genestudioconnector/connect.java
@@ -0,0 +1,79 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.clothocad.widget.genestudioconnector;
+import org.clothocore.api.plugin.ClothoWidget;
+import java.io.*;
+import javax.swing.*;
+/**
+ *
+ * @author USTC_Software
+ */
+public class connect implements ClothoWidget{
+    File f;
+    FileReader fr;
+    BufferedReader br;
+
+    @Override
+    public void launch() {
+        try{
+        //String tmp=this.prepare();+" -from-clotho";
+        Runtime.getRuntime().exec(this.prepare());
+        }
+        catch(Exception e){
+            JOptionPane.showConfirmDialog(null, "Cannot start GeneStudio please reconfigure it!","Launch Failed!",JOptionPane.CLOSED_OPTION,JOptionPane.WARNING_MESSAGE);
+        }
+       // throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void close() {
+        try{
+        String tmp=this.prepare()+" -close";
+        Runtime.getRuntime().exec(tmp);
+        }
+        catch(Exception e){
+            JOptionPane.showConfirmDialog(null, "Cannot start GeneStudio please reconfigure it!","Launch Failed!",JOptionPane.CLOSED_OPTION,JOptionPane.WARNING_MESSAGE);
+        }
+        //throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void init() {
+       // throw new UnsupportedOperationException("Not supported yet.");
+    }
+    public String prepare() throws Exception{
+        String tmp=null;
+        f=new File(this.GetPath());
+        fr=new FileReader(f);
+        br=new BufferedReader(fr);
+        br.readLine();
+        tmp=br.readLine();
+        tmp=tmp.substring(8,tmp.length());
+        this.br.close();
+        this.fr.close();
+        return tmp;
+    }
+    
+
+    public String GetPath(){
+        String osname=System.getProperty("os.name");
+        if(osname.equals("Windows 7")){
+            return "C:/Users/farseer/Desktop/files/confg.txt";
+        }
+        else if(osname.equals("Windows  XP"))
+        {
+             return "C:/Users/farseer/Desktop/files/confg.txt";
+        }
+        else if(osname.equals("Windows  XP")){
+             return "C:/Users/farseer/Desktop/files/confg.txt";
+        }
+        else if(osname.equals("Linux")){
+            return "C:/Users/farseer/Desktop/files/confg.txt";
+        }
+        else return null;
+    }
+
+}
