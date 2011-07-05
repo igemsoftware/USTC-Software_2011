0789da903c68f5ceda24d7228fbe1ade5b96df7e
crlf0710 <crlf0710@gmail.com>
2011-07-05 20:54:08 +0800

    minor fix

--- src/designer/models/reactionnetworkmodel/ReactionNetworkSBMLImportProxy.cpp
+++ src/designer/models/reactionnetworkmodel/ReactionNetworkSBMLImportProxy.cpp
@@ -24,12 +24,55 @@ modelObjectIndex ReactionNetworkSBMLImportProxy::createModelObject(modelObjectIn
     QDomElement *domElem = static_cast<QDomElement*>(data);
     if(!domElem)
         return 0;
-//    [](int x){ return x*x;}(5);
-    QScriptValue newModelObject = getEngine()->newObject();
-    itemPool.append(newModelObject);
 
-    printf(domElem->nodeName().toLatin1().data());
-    return itemPool.count()-1;
+    struct handlers
+    {
+        static QString collectChildrenAsString(QDomElement& elem)
+        {
+            return "";
+        }
+    };
+    if(domElem->nodeName()=="notes")
+    {
+        QString text;
+        QTextStream textStream(&text);
+        domElem->save(textStream, 0);
+
+        itemPool[parent].setProperty("*notes*", text);
+
+        return parent;
+    }
+    else if(domElem->nodeName()=="annotation")
+    {
+        QString text;
+        QTextStream textStream(&text);
+        domElem->save(textStream, 0);
+
+        itemPool[parent].setProperty("*annotation*", domElem->toText().data());
+        return parent;
+    }
+    else
+    {
+        QScriptValue newModelObject = getEngine()->newObject();
+        modelObjectIndex newModelObjectIndex = itemPool.count();
+        itemPool.append(newModelObject);
+
+        newModelObject.setProperty("*name*", domElem->nodeName());
+
+        for(int attrIndex = 0 ; attrIndex < domElem->attributes().count(); attrIndex++)
+        {
+            newModelObject.setProperty(  domElem->attributes().item(attrIndex).nodeName(),
+                                         domElem->attributes().item(attrIndex).nodeValue());
+        }
+
+        for(QDomElement childElem = domElem->firstChildElement();
+            !childElem.isNull(); childElem = childElem.nextSiblingElement())
+        {
+            createModelObject(newModelObjectIndex, &childElem);
+        }
+
+        return newModelObjectIndex;
+    }
 }
 
 void ReactionNetworkSBMLImportProxy::setModelObjectProperty(modelObjectIndex index, QString propertyName, QString value)
