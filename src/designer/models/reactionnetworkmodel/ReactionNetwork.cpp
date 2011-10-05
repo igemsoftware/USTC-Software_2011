ad9cca0e60cfff0a9e38cf322c42704cf3556455
CrLF0710 <crlf0710@gmail.com>
2011-10-05 15:12:16 +0800

    Temporarily disable unfinished stuff

--- src/designer/models/reactionnetworkmodel/ReactionNetwork.cpp
+++ src/designer/models/reactionnetworkmodel/ReactionNetwork.cpp
@@ -12,12 +12,15 @@ QStringList ReactionNetworkModel::getSupportedViewList() const
     QStringList supportedViewList;    
     supportedViewList.append("NetworkView");
     supportedViewList.append("BehaviorView");
+	
+/*
     supportedViewList.append("SBMLEditorView");
     //! \bug For debug purpose.
     supportedViewList.append("Plot3DView");
     supportedViewList.append("ODEView");
     supportedViewList.append("ClothoDBView");
     supportedViewList.append("WebPageView");
+*/
 
     return supportedViewList;
 }
